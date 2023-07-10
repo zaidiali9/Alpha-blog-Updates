2d04063025ae0d2a598c391a40b22ba3914bb952
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2023-07-10 14:09:46 +0500

    Added Secure Password to DataBase

---
+++ db/migrate/20230710083116_add_password_digist_to_users.rb
@@ -0,0 +1,5 @@
+class AddPasswordDigistToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_204457) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_083116) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_204457) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digist"
+    t.string "password_digest"
   end
 
 end
