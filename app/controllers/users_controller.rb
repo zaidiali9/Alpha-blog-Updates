ced97b42530a38ba7abbdc8b316ec22c47742874
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2023-07-10 16:12:51 +0500

    Created Signup Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user=User.new
+  end
+
+  def user_params
+      params.require(:user).permit(:username, :email, :password)
+  end
+
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      flash[:notice]="Welcome to Alpha-Blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :users
   resources :articles
   root 'home#index'
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
