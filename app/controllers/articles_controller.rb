64b53a02bfd553e776925f71688eeb606a59e3f5
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2023-07-11 15:47:35 +0500

    Admin Functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -71,7 +71,7 @@ class ArticlesController < ApplicationController
     end
 
   def require_same_user
-    if current_user!=@article.user
+    if current_user!=@article.user && !current_user.admin?
       flash[:notice] = "You can Only Edit Your Own Article"
       redirect_to @article
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_083116) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_095107) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_083116) do
     t.datetime "updated_at", null: false
     t.string "password_digist"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
