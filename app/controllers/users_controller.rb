78888db56b7889971cec468405728ec0345bb6e9
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2023-07-11 14:43:58 +0500

    Delete User Functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
+
+  before_action :require_user, only: [:edit, :update]
   def new
     @user=User.new
   end
 
+  def destroy
+    @user=User.find(params[:id])
+    if @user == current_user
+      @user.destroy
+      session[:user_id]=nil
+      redirect_to articles_path
+    end
+  end
   def user_params
       params.require(:user).permit(:username, :email, :password)
   end
@@ -20,15 +30,21 @@ class UsersController < ApplicationController
 
   def edit
     @user=User.find(params[:id])
+    if current_user!= @user
+      flash[:notice] = "You can Only Edit Your Own Account"
+      redirect_to @user
+    end
   end
 
   def update
     @user =User.find(params[:id])
-    if @user.update(user_params)
-      flash[:notice] ="Your Account is Updated"
-      redirect_to @user
-    else
-      render 'edit'
+    if @user==current_user
+      if @user.update(user_params)
+        flash[:notice] ="Your Account is Updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
     end
 
   end
